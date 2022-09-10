e91662df76bee820bcf6518d9f1f847124aba93c
alireza alizadeh <alireza.aa9700@gmail.com>
2022-09-10 16:20:07 +0430

    imputable

--- Counter.sol
+++ Counter.sol
@@ -6,18 +6,19 @@ contract Counter {
     uint256 public count;
 
     // Function to get the current count
-    function get() public view returns (uint256) {
-        // the current count is:
-        return count;
-    }
+    // function get() public view returns (uint256) {
+    //     // the current count is:
+    //     return count;
+    // }
+    // count public don't need to get function
 
     // function to increament count by 1
-    function increase() public {
-        count += 1;
+    function increase(uint256 _elevator) public {
+        count += _elevator;
     }
 
     // function to decreament count by 1
-    function decrease() public {
-        count -= 1;
+    function decrease(uint256 _reducer) public {
+        count -= _reducer;
     }
 }
