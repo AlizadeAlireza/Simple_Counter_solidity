211920cef70bc9c7294bc56861ada8a77efec6bf
alireza alizadeh <alireza.aa9700@gmail.com>
2022-09-11 11:29:24 +0430

    negative result

--- Counter.sol
+++ Counter.sol
@@ -3,22 +3,22 @@
 pragma solidity ^0.8.13;
 
 contract Counter {
-    uint256 public count;
+    int256 public count;
 
     // Function to get the current count
-    // function get() public view returns (uint256) {
+    // function get() public view returns (int256) {
     //     // the current count is:
     //     return count;
     // }
     // count public don't need to get function
 
     // function to increament count by 1
-    function increase(uint256 _elevator) public {
+    function increase(int256 _elevator) public {
         count += _elevator;
     }
 
     // function to decreament count by 1
-    function decrease(uint256 _reducer) public {
+    function decrease(int256 _reducer) public {
         count -= _reducer;
     }
 }
