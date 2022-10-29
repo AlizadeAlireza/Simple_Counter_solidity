343af3a5fad1447d0303ea843780a621bc8e2bc1
Ali_Elahi <93305207+ElahiAli@users.noreply.github.com>
2022-10-29 16:44:28 +0330

    Upstream (#4)
    * change modifier
    
    * update : add fundMe
    
    * updated
    
    * updated
    
    Co-authored-by: ali elahi <alielahi090@gmail.com>

--- Counter.sol
+++ Counter.sol
@@ -1,25 +1,33 @@
 // SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
 
-pragma solidity ^0.8.13;
+import "./PriceConverter.sol";
 
 contract Counter {
+    using PriceConverter for uint256;
     int256 public count;
-    
-    
-    // Function to get the current count
-    // function get() public view returns (int256) {
-    //     // the current count is:
-    //     return count;
-    // }
-    // count public don't need to get function
-    
-    
-    
-    modifier firstNeed {
-        require(count != 0 ,"Please increase or decrease the count first.");
+    uint256 public constant MINIMUMUSD = 1 * 1e18;
+    address[] public funders;
+    address public immutable owner;
+
+    modifier firstNeed() {
+        require(securityCheck(msg.sender), "Please charge the contract.");
+        require(count != 0, "Please increase or decrease the count first.");
         _;
     }
-    
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function fundMe() public payable {
+        require(
+            msg.value.getConversionRate() >= MINIMUMUSD,
+            "Didn't send enouph ETH!"
+        );
+        funders.push(msg.sender);
+    }
+
     // function to increament count by 1
     function increaseByOne() public {
         count += 1;
@@ -29,26 +37,33 @@ contract Counter {
     function decreaseByOne() public {
         count -= 1;
     }
-    
+
     // function to increament count by given number
-    function increase(int256 _elevator) public{
+    function increase(int256 _elevator) public firstNeed {
         count += _elevator;
     }
 
     // function to decreament count by given number
-    function decrease(int256 _reducer) public{
+    function decrease(int256 _reducer) public firstNeed {
         count -= _reducer;
     }
 
-    
     // function to multiply count by given number
-    function multiply(int256 _modulus) firstNeed public{
+    function multiply(int256 _modulus) public firstNeed {
         count *= _modulus;
     }
 
     // function to divide count by given number
-    function divide(int256 _div) firstNeed public{
+    function divide(int256 _div) public firstNeed {
         count /= _div;
     }
-}
 
+    function securityCheck(address name) internal view returns (bool) {
+        for (uint256 data = 0; data < funders.length; data++) {
+            if (funders[data] == name) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ PriceConverter.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier:MIT
+pragma solidity ^0.8.13;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+library PriceConverter {
+    function getPrice() internal view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
+        );
+
+        (, int answer, , , , ) = priceFeed.latestRoundData();
+        return uint256(answer * 1e10);
+    }
+
+    function getConversionRate(ethAmount) internal view returns (uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethAmount * ethPrice) / 1e18;
+        return ethAmountInUsd;
+    }
+}
